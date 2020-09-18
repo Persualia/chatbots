3f5307945c17592210dce509190733de33db3505
al6ert <albertperezrabella@hotmail.com>
2020-09-18 11:02:31 +0200

    test body.js

---
+++ public/js/body.js
@@ -0,0 +1,37 @@
+/* Funciones para V3 */
+
+  var landbotScope = this;      
+  this.onLoad(function() {  	
+    landbotScope.setCustomData({ businessisopen: isBusinessOpen() });
+    if (typeof(calendar) != undefined) {
+        landbotScope.setCustomData({ mobile: isMobile() });
+    }  	
+});
+
+
+/* Check For Business Hours */
+function isBusinessOpen() {
+  var nowDateTime = new Date();
+  var offset = 0;
+
+  var nowYear = nowDateTime.getFullYear();
+  var nowMonth = nowDateTime.getMonth();
+  var nowDate = nowDateTime.getDate();
+  var nowDay = nowDateTime.getDay()  
+  var currentStatus = 'close'; 
+  var openDay = calendar[nowDay];
+  if (openDay) { 
+  	openDay.forEach(function (item, index){
+  	var trackDate = new Date(nowYear, nowMonth, nowDate, item.h, item.m); 
+  	if (nowDateTime > trackDate) currentStatus = item.status;
+	});
+} 
+  console.log('open: '+currentStatus);
+  return (currentStatus == 'open');
+  //return(currentStatus);
+} 
+  
+/* Check Mobile or Desktop */  
+function isMobile(){
+	return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
--- public/js/head.js
+++ public/js/head.js
@@ -1,19 +1,26 @@
 /* Google Tag Manager */
 
-(function (w, d, s, l, i) {
-    w[l] = w[l] || []; w[l].push({
-        'gtm.start':
-            new Date().getTime(), event: 'gtm.js'
-    }); var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', gtmID);
+if (typeof gtmID !== 'undefined') {
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', gtmID);
+}
 
 /* End Google Tag Manager */
 
 /* Check if is Iframe */
 function isIframe() {
     return (window.self !== window.top)
+}
+
+/* Check Mobile or Desktop */  
+function isMobile(){
+	return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
 /*send to Datalayer even is in iframe */
