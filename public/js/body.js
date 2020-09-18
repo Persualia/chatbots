6d4093dede261a311d4496d17835c08591214dfb
al6ert <albertperezrabella@hotmail.com>
2020-09-18 11:30:47 +0200

    fix

--- public/js/body.js
+++ public/js/body.js
@@ -1,28 +1,2 @@
 /* Funciones para V3 */
 
-/* Check For Business Hours */
-function isBusinessOpen() {
-    var nowDateTime = new Date();
-    var offset = 0;
-
-    var nowYear = nowDateTime.getFullYear();
-    var nowMonth = nowDateTime.getMonth();
-    var nowDate = nowDateTime.getDate();
-    var nowDay = nowDateTime.getDay()
-    var currentStatus = 'close';
-    var openDay = calendar[nowDay];
-    if (openDay) {
-        openDay.forEach(function (item, index) {
-            var trackDate = new Date(nowYear, nowMonth, nowDate, item.h, item.m);
-            if (nowDateTime > trackDate) currentStatus = item.status;
-        });
-    }
-    console.log('open: ' + currentStatus);
-    return (currentStatus == 'open');
-    //return(currentStatus);
-}
-
-/* Check Mobile or Desktop */
-function isMobile() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
--- public/js/head.js
+++ public/js/head.js
@@ -23,6 +23,33 @@ function isMobile(){
 	return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
+/* Check For Business Hours */
+function isBusinessOpen() {
+    var nowDateTime = new Date();
+    var offset = 0;
+
+    var nowYear = nowDateTime.getFullYear();
+    var nowMonth = nowDateTime.getMonth();
+    var nowDate = nowDateTime.getDate();
+    var nowDay = nowDateTime.getDay()
+    var currentStatus = 'close';
+    var openDay = calendar[nowDay];
+    if (openDay) {
+        openDay.forEach(function (item, index) {
+            var trackDate = new Date(nowYear, nowMonth, nowDate, item.h, item.m);
+            if (nowDateTime > trackDate) currentStatus = item.status;
+        });
+    }
+    console.log('open: ' + currentStatus);
+    return (currentStatus == 'open');
+    //return(currentStatus);
+}
+
+/* Check Mobile or Desktop */
+function isMobile() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
 /*send to Datalayer even is in iframe */
 function dataLayerEvent(data) {
     if (isIframe()) {
