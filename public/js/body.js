d3104cff1de767d4f2ed561a7d58bb9a4645a1c8
al6ert <albertperezrabella@hotmail.com>
2020-09-22 10:34:33 +0200

    fix

--- public/js/body.js
+++ public/js/body.js
@@ -1,7 +1,8 @@
 /* Funciones para V3 */
-
+console.log("body file");
 console.log(landbotScope);
 landbotScope.onLoad(function () {
+    console.log("landbotScope onLoad");
     if (typeof (calendar) != "undefined") {
         console.log("defined " + typeof (calendar));
         landbotScope.setCustomData({ businessisopen: isBusinessOpen() });
@@ -9,3 +10,7 @@ landbotScope.onLoad(function () {
     else console.log("undefined " + typeof (calendar));
     landbotScope.setCustomData({ mobile: isMobile() });
 });
+
+this.onload(function (){
+    console.log("file onLoad");
+})
