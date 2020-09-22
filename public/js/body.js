ec7f7bd7519ae444d37e372217976fa24a4482a7
al6ert <albertperezrabella@hotmail.com>
2020-09-22 10:20:22 +0200

    fix

--- public/js/body.js
+++ public/js/body.js
@@ -1,6 +1,6 @@
 /* Funciones para V3 */
 
-
+console.log(landbotScope);
 landbotScope.onLoad(function () {
     if (typeof (calendar) != "undefined") {
         console.log("defined " + typeof (calendar));
