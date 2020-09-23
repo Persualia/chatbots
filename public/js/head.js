c00094beb82268649b25b850c30d9dd6f22bbaf4
al6ert <albertperezrabella@hotmail.com>
2020-09-23 13:23:25 +0200

    fix

--- public/js/head.js
+++ public/js/head.js
@@ -1,24 +1,3 @@
-/* Google Tag Manager */
-
-if (typeof gtmID !== 'undefined') {
-    (function (w, d, s, l, i) {
-        w[l] = w[l] || []; w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        }); var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', gtmID);
-}
-
-if (!isIframe()) {
-    if (typeof landbotName !== 'undefined')
-        dataLayerEvent({'event':'Landbot Name','landbotName':landbotName}); 
-    dataLayerEvent({'event':'Ace','action':'LPV Bot'});
-}
-
-/* End Google Tag Manager */
-
 /* Check if is Iframe */
 function isIframe() {
     return (window.self !== window.top)
@@ -93,3 +72,23 @@ window.saveVariables = function(landbotScope, variables = null) {
     }
 }
 
+/* Google Tag Manager */
+
+if (typeof gtmID !== 'undefined') {
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', gtmID);
+}
+
+if (!isIframe()) {
+    if (typeof landbotName !== 'undefined')
+        dataLayerEvent({'event':'Landbot Name','landbotName':landbotName}); 
+    dataLayerEvent({'event':'Ace','action':'LPV Bot'});
+}
+
+/* End Google Tag Manager */
