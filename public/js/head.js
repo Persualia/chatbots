c935d7b5473ef6019597ed0832c097a026e290ca
al6ert <albertperezrabella@hotmail.com>
2020-09-25 12:41:18 +0200

    fix

--- public/js/head.js
+++ public/js/head.js
@@ -72,7 +72,7 @@ function saveVariables(landbotScope, variables = null) {
         for (const [key, value] of Object.entries(variables)) {
             console.log(typeof key + " " + key);
             console.log(typeof value + " " + value);
-            landbotScope.setCustomData({ key: value });
+            console.log(landbotScope.setCustomData({ key: value }));
         }
     }
 }
