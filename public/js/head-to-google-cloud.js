ae516a5ea279d537e899d1acc119a5869e632209
al6ert <albertperezrabella@hotmail.com>
2020-10-21 11:18:12 +0200

    1rst attemp head-to-google-cloud

---
+++ public/js/head-to-google-cloud.js
@@ -0,0 +1,22 @@
+async function openSlackChannel(data){
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    console.log(data);
+
+    var raw = JSON.stringify({"slackchannelinfo":data});//JSON.stringify({"account":"persualia","userId":"56063398","botId":"725126","node":"Nkf80ivkn"});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://us-central1-landbot-persualia.cloudfunctions.net/createSlackChannelFromLandbot", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+}
