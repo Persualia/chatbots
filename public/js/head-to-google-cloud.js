98053b387d94f7cec50bc934798e73b30341a8d2
al6ert <albertperezrabella@hotmail.com>
2020-10-22 09:53:11 +0200

    moved url to europe-west-1

--- public/js/head-to-google-cloud.js
+++ public/js/head-to-google-cloud.js
@@ -1,11 +1,7 @@
 async function openSlackChannel(data){
-
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");    
-    console.log(data);
-
-    var raw = JSON.stringify({"slackchannelinfo":data});//JSON.stringify({"account":"persualia","userId":"56063398","botId":"725126","node":"Nkf80ivkn"});
-
+    var raw = JSON.stringify({"slackchannelinfo":data});
     var requestOptions = {
     mode: 'no-cors',
     method: 'POST',
@@ -14,7 +10,7 @@ async function openSlackChannel(data){
     redirect: 'follow'
     };
 
-    fetch("https://us-central1-landbot-persualia.cloudfunctions.net/createSlackChannelFromLandbot", requestOptions)
+    fetch("https://europe-west1-landbot-persualia.cloudfunctions.net/createSlackChannelFromLandbot", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
