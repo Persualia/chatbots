f6b0710d16b854056a9ecb9b41f9b531044b5c83
al6ert <albertperezrabella@hotmail.com>
2020-10-23 09:19:15 +0200

    adding send to google sheet

--- public/js/head-to-google-cloud.js
+++ public/js/head-to-google-cloud.js
@@ -16,3 +16,22 @@ async function openSlackChannel(data){
     .catch(error => console.log('error', error));
 
 }
+
+async function sendToGoogleSheet(data) {    
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");    
+      var raw = JSON.stringify(data);
+      var requestOptions = {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+      };
+  
+      fetch("https://europe-west1-landbot-persualia.cloudfunctions.net/createSlackChannelFromLandbot", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    
+}
