441545050c573293c7447194deb76245e92e1a31
al6ert <albertperezrabella@hotmail.com>
2020-11-10 10:30:04 +0100

    fix

--- public/js/head-to-google-cloud.js
+++ public/js/head-to-google-cloud.js
@@ -48,17 +48,14 @@ function sendToGoogleSheet(data) {
             body: raw
         };
         var request = new Request("https://europe-west1-landbot-persualia.cloudfunctions.net/sendToGoogleSheet", requestOptions);
-        fetch(request).then(response => {
-            console.log('inside the response');
-            console.log(response);
-            console.log(JSON.stringify(response));
-            response.json().then(data => {
-                console.log('inside the json data');
-                console.log(data);
-                return data;
+        fetch(request)
+            .then(response => {
+                console.log('inside the response');
+                console.log(response);
+                console.log(JSON.stringify(response));
+                response.body.json()            
             })
-            
-        });        
+            .then(json => console.log(json) );        
         console.log('step after fetch');
     } catch (error) {
         console.log('error', error);
