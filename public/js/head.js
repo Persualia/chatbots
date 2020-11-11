a2e5d7ef2f995a800f52e6c99127c8a97a9918df
al6ert <albertperezrabella@hotmail.com>
2020-11-11 11:19:25 +0100

    fix

--- public/js/head.js
+++ public/js/head.js
@@ -36,8 +36,9 @@ function isBusinessOpen(key = null) {
 /*send to Datalayer even is in iframe */
 function dataLayerEvent(data) {
     if (data.event.match('survey','i')) {
+        console.log('survey match');
         data.category = window.landbotName;
-    }
+    } else console.log('no match survey');
     dataLayer.push(data);
 }
 
